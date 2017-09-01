38c8217080e07517fb52256ac3bc8f5a537c8305
teddylockhart <teddy.lockhart@gmail.com>
2017-08-31 23:56:14 -0700

    3rd: Added Firebase, broke Moment

--- choochooLogic.js
+++ choochooLogic.js
@@ -8,24 +8,36 @@ var config = {
   messagingSenderId: "619923435321"
 };
 
-
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// var tFrequency = 90;
+
+// var startTrain = snapshot.val().FirstTrain;
+
+// Moment.js train time calculations
+// var firstTimeConverted = moment(startTrain, "hh:mm").subtract(1, "years");
+// var currentTime = moment();
+// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+// var tRemainder = diffTime % tFrequency;
+// var tMinutesTillTrain = tFrequency - tRemainder;
+// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+// Writes Train Table
 database.ref("/cTrain").on("child_added", function(snapshot){
 
   var tableString = "<tr><td>" + snapshot.val().TrainName + 
         "</td><td>" + snapshot.val().Destination +
-        "</td><td>" + snapshot.val().FirstTrain +
         "</td><td>" + snapshot.val().Frequency +
+        // "</td><td>" + tMinutesTillTrain +
         "</td></tr>";
 
   $("#train-table").append(tableString);
 
 });
 
-
+// Takes User Input and Stores to Firebase
 $("#submit").on("click", function() {
   event.preventDefault();
 
