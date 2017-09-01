eea649a7cec5b58765bfe63f31119d3de34bfc76
teddylockhart <teddy.lockhart@gmail.com>
2017-09-01 10:11:13 -0700

    4th: Added Moment.js, needs correction calculations

--- choochooLogic.js
+++ choochooLogic.js
@@ -12,25 +12,25 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// var tFrequency = 90;
-
-// var startTrain = snapshot.val().FirstTrain;
-
-// Moment.js train time calculations
-// var firstTimeConverted = moment(startTrain, "hh:mm").subtract(1, "years");
-// var currentTime = moment();
-// var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-// var tRemainder = diffTime % tFrequency;
-// var tMinutesTillTrain = tFrequency - tRemainder;
-// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+var tFrequency = 90;
 
 // Writes Train Table
 database.ref("/cTrain").on("child_added", function(snapshot){
+  var startTrain = snapshot.val().FirstTrain;
+
+  // Moment.js train time calculations
+  var firstTimeConverted = moment(startTrain, "hh:mm").subtract(1, "years");
+  var currentTime = moment();
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  var tRemainder = diffTime % tFrequency;
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
   var tableString = "<tr><td>" + snapshot.val().TrainName + 
         "</td><td>" + snapshot.val().Destination +
         "</td><td>" + snapshot.val().Frequency +
-        // "</td><td>" + tMinutesTillTrain +
+        "</td><td>" + nextTrain +
+        "</td><td>" + tMinutesTillTrain +
         "</td></tr>";
 
   $("#train-table").append(tableString);
